bf1200c8a8d5a8bb8ae670bb0e0d161c3f79272f
Gonzalo García <gonzalo@neoland.es>
2020-10-22 12:12:05 +0200

    * Changing parameter input from child

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  nombre: string = 'Nombre del usuario';
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shared/hijo/hijo.component.ts
+++ src/app/shared/hijo/hijo.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hijo',
   templateUrl: './hijo.component.html',
   styleUrls: ['./hijo.component.css']
 })
+
 export class HijoComponent implements OnInit {
+
+  // Definir el parámetro de entrada que me va a permitir recibir info desde el padre
+  @Input() parametroEntrada: string = "Sin nombre";
 
   constructor() { }
 
