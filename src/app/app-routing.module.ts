17e6adb2a86c1162bfb1ba3257b83ea1a9ec8f76
Gonzalo García <gonzalo@neoland.es>
2020-10-22 11:18:07 +0200

    * Creating child components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'homepage', component: HomeComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: "**", redirectTo: 'homepage'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HijoComponent } from './shared/hijo/hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    HijoComponent
   ],
   imports: [
     BrowserModule,
