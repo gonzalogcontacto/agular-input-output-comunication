0abb8c143ca5ae92c9ac0d1ae07f69cb15f70661
Gonzalo García <gonzalo@neoland.es>
2020-10-22 12:43:14 +0200

    * emmiting a change from child to the parent

--- src/app/shared/hijo/hijo.component.ts
+++ src/app/shared/hijo/hijo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-hijo',
@@ -10,10 +10,24 @@ export class HijoComponent implements OnInit {
 
   // Definir el parámetro de entrada que me va a permitir recibir info desde el padre
   @Input() parametroEntrada: string = "Sin nombre";
+    
+  // Definir el parámetro de salida que me va a permitir recibir info en el padre
+  @Output() parametroSalida = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  cambiarPropiedad(){
+    console.log('Click en el button del hijo');
+    this.parametroEntrada = 'Ana Belén'
+
+    //Aviso al padre , Ejecutando el evento de salida y pasándole el string que he modificado
+    this.parametroSalida.emit(this.parametroEntrada);
+
+  }
+
+
 
 }
